b412cb11fff658b1b64945e0f7b28227ea3dfaaf
Min Cai <min.cai.china@gmail.com>
2020-09-01 21:17:31 +0800

    Update SimpleFileInput.

--- lib/components/SimpleForm/Inputs/SimpleFileInput/SimpleFileInput.tsx
+++ lib/components/SimpleForm/Inputs/SimpleFileInput/SimpleFileInput.tsx
@@ -1,8 +1,23 @@
+import React, {ChangeEvent} from "react";
 import {Component} from "react";
+import { Form } from "react-bootstrap";
 import {SimpleFileInputProps} from "./SimpleFileInputProps";
 
 export class SimpleFileInput extends Component<SimpleFileInputProps, any> {
     render() {
-        return undefined;
+        return (
+            <Form.Group controlId={this.props.source}>
+                <Form.File
+                    name={this.props.source}
+                    label={this.props.label}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => this.handleChange(e)}
+                />
+            </Form.Group>
+        );
+    }
+
+    private handleChange(e: ChangeEvent<HTMLInputElement>) {
+        let files = Array.from(e.target.files ?? []);
+        console.log("files:", files);
     }
 }
--- lib/components/SimpleForm/Inputs/SimpleFileInput/SimpleFileInputProps.tsx
+++ lib/components/SimpleForm/Inputs/SimpleFileInput/SimpleFileInputProps.tsx
@@ -1,2 +1,7 @@
+import React from "react";
+
 export interface SimpleFileInputProps {
+    label: React.ReactNode;
+    record: any;
+    source: string;
 }
