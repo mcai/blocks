db327201a31cf631f8456573a4fac78871657a65
Min Cai <min.cai.china@gmail.com>
2020-09-01 20:10:25 +0800

    Update SimpleSelectInput.tsx.

--- lib/components/SimpleForm/Inputs/SimpleSelectInput/SimpleSelectInput.tsx
+++ lib/components/SimpleForm/Inputs/SimpleSelectInput/SimpleSelectInput.tsx
@@ -1,8 +1,21 @@
-import {Component} from "react";
+import React, {Component} from "react";
 import {SimpleSelectInputProps} from "./SimpleSelectInputProps";
+import {Form} from "react-bootstrap";
 
 export class SimpleSelectInput extends Component<SimpleSelectInputProps, any> {
     render() {
-        return undefined;
+        return (
+            <Form.Group controlId={this.props.source}>
+                <Form.Label>{this.props.label}</Form.Label>
+
+                <Form.Control as="select" name={this.props.source} value={this.props.record[this.props.source]}>
+                    {
+                        this.props.options.map(option => (
+                            <option value={option.value}>{option.label}</option>
+                        ))
+                    }
+                </Form.Control>
+            </Form.Group>
+        );
     }
 }
--- lib/components/SimpleForm/Inputs/SimpleSelectInput/SimpleSelectInputProps.tsx
+++ lib/components/SimpleForm/Inputs/SimpleSelectInput/SimpleSelectInputProps.tsx
@@ -1,2 +1,8 @@
+import React from "react";
+
 export interface SimpleSelectInputProps {
+    label: React.ReactNode;
+    record: any;
+    source: string;
+    options: {value: string, label: string}[]
 }
