e8a9f5d7bcff8729327917154d2b1cd4385d7480
Min Cai <min.cai.china@gmail.com>
2020-09-01 19:50:00 +0800

    Update SimpleBooleanInput.

--- lib/components/SimpleForm/Inputs/SimpleBooleanInput/SimpleBooleanInput.tsx
+++ lib/components/SimpleForm/Inputs/SimpleBooleanInput/SimpleBooleanInput.tsx
@@ -5,16 +5,8 @@ import {Form} from "react-bootstrap";
 export class SimpleBooleanInput extends Component<SimpleBooleanInputProps, any> {
     render() {
         return (
-            <Form.Group controlId={this.props.name}>
-                <Form.Label>{this.props.title}</Form.Label>
-
-                <Form.Check type="checkbox" name={this.props.name} checked={this.props.value} label={this.props.title} />
-
-                {
-                    this.props.hint && <Form.Text className="text-muted">
-                        {this.props.hint}
-                    </Form.Text>
-                }
+            <Form.Group controlId={this.props.source}>
+                <Form.Check type="checkbox" name={this.props.source} checked={this.props.record[this.props.source]} label={this.props.label} />
             </Form.Group>
         );
     }
--- lib/components/SimpleForm/Inputs/SimpleBooleanInput/SimpleBooleanInputProps.tsx
+++ lib/components/SimpleForm/Inputs/SimpleBooleanInput/SimpleBooleanInputProps.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 export interface SimpleBooleanInputProps {
-    name: string;
-    title?: React.ReactNode;
-    value?: boolean;
-    placeholder?: string;
-    hint?: React.ReactNode;
+    label: React.ReactNode;
+    record: any;
+    source: string;
 }
