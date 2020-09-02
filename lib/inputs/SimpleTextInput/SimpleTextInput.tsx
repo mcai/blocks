02d1f39954b41e2b6003c1fb884175eaff3ef00a
Min Cai <min.cai.china@gmail.com>
2020-09-02 16:24:21 +0800

    Update SimpleTextInput.tsx.

--- lib/inputs/SimpleTextInput/SimpleTextInput.tsx
+++ lib/inputs/SimpleTextInput/SimpleTextInput.tsx
@@ -1,14 +1,15 @@
 import React, {Component} from "react";
 import {SimpleTextInputProps} from "./SimpleTextInputProps";
 import {Field} from "formik";
+import { Form } from "react-bootstrap";
 
 export class SimpleTextInput extends Component<SimpleTextInputProps, any> {
     render() {
         return (
-            <div>
-                <label>{this.props.label}</label>
+            <Form.Group>
+                <Form.Label>{this.props.label}</Form.Label>
                 <Field type={this.props.password ? "password" : "text"} name={this.props.name} placeholder={this.props.placeholder}/>
-            </div>
+            </Form.Group>
         );
     }
 }
