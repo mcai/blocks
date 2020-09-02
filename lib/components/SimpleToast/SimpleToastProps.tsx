0cf46895f150912b0de32cdb239fb2f3809f6840
Min Cai <min.cai.china@gmail.com>
2020-09-02 17:28:29 +0800

    Add SimpleToast.

--- lib/components/SimpleToast/SimpleToastProps.tsx
+++ lib/components/SimpleToast/SimpleToastProps.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export interface SimpleToastProps {
     visible: boolean
-    onClose: () => {}
+    onClose: () => void
 
     title?: React.ReactNode
     subtitle?: React.ReactNode
