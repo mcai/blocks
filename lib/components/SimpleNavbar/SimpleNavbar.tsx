ff96c934460688f817960244932c017031601540
Min Cai <min.cai.china@gmail.com>
2020-09-03 14:41:51 +0800

    Update SimpleNavbar.tsx.

--- lib/components/SimpleNavbar/SimpleNavbar.tsx
+++ lib/components/SimpleNavbar/SimpleNavbar.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {SimpleNavbarProps} from "./SimpleNavbarProps";
 
 export class SimpleNavbar extends Component<SimpleNavbarProps, any> {
@@ -9,27 +9,29 @@ export class SimpleNavbar extends Component<SimpleNavbarProps, any> {
 
     render() {
         return (
-            <Navbar bg="primary" variant="dark">
-                <Navbar.Brand href={this.props.brand.href}>{this.props.brand.title}</Navbar.Brand>
-                <Nav className="mr-auto">
-                    {
-                        this.props.sections.map(section => (
-                            <NavDropdown title={section.title} id={section.id}>
-                                {
-                                    section.items.map(item => (
-                                        <NavDropdown.Item
-                                            href={item.href}
-                                            key={item.key}
-                                        >
-                                            {item.title}
-                                        </NavDropdown.Item>
-                                    ))
-                                }
-                            </NavDropdown>
-                        ))
-                    }
-                </Nav>
-            </Navbar>
+            <Container className={"mb-3"}>
+                <Navbar bg="primary" variant="dark">
+                    <Navbar.Brand href={this.props.brand.href}>{this.props.brand.title}</Navbar.Brand>
+                    <Nav className="mr-auto">
+                        {
+                            this.props.sections.map(section => (
+                                <NavDropdown title={section.title} id={section.id}>
+                                    {
+                                        section.items.map(item => (
+                                            <NavDropdown.Item
+                                                href={item.href}
+                                                key={item.key}
+                                            >
+                                                {item.title}
+                                            </NavDropdown.Item>
+                                        ))
+                                    }
+                                </NavDropdown>
+                            ))
+                        }
+                    </Nav>
+                </Navbar>
+            </Container>
         );
     }
 }
