601ad371b69cb7215af1025db5652f033cd93749
Min Cai <min.cai.china@gmail.com>
2020-09-03 14:38:54 +0800

    Update SimpleApp.tsx.

--- lib/components/SimpleApp/SimpleApp.tsx
+++ lib/components/SimpleApp/SimpleApp.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {SimpleAppProps} from "./SimpleAppProps";
 import {SimpleAppState} from "./SimpleAppState";
@@ -8,23 +8,24 @@ import {SimpleToast} from "../SimpleToast/SimpleToast";
 export class SimpleApp extends Component<SimpleAppProps, SimpleAppState>{
     render() {
         return (
-            <Container>
+            <Fragment>
                 <SimpleToast/>
+                {this.props.navbar}
 
                 <BrowserRouter basename={process.env.PUBLIC_URL}>
-                    {this.props.navbar}
-
-                    <Switch>
-                        {
-                            this.props.routes.map(route => (
-                                <Route exact path={route.path}>
-                                    {route.page}
-                                </Route>
-                            ))
-                        }
-                    </Switch>
+                    <Container>
+                        <Switch>
+                            {
+                                this.props.routes.map(route => (
+                                    <Route exact path={route.path}>
+                                        {route.page}
+                                    </Route>
+                                ))
+                            }
+                        </Switch>
+                    </Container>
                 </BrowserRouter>
-            </Container>
+            </Fragment>
         );
     }
 }
