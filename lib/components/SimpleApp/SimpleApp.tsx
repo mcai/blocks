dc1ca7e891010b2a7eebc13005e3eee451e9af7b
Min Cai <min.cai.china@gmail.com>
2020-09-03 14:29:16 +0800

    Update SimpleApp.tsx.

--- lib/components/SimpleApp/SimpleApp.tsx
+++ lib/components/SimpleApp/SimpleApp.tsx
@@ -3,11 +3,14 @@ import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {SimpleAppProps} from "./SimpleAppProps";
 import {SimpleAppState} from "./SimpleAppState";
 import {Container} from "react-bootstrap";
+import {SimpleToast} from "../SimpleToast/SimpleToast";
 
 export class SimpleApp extends Component<SimpleAppProps, SimpleAppState>{
     render() {
         return (
             <Container>
+                <SimpleToast/>
+
                 <BrowserRouter basename={process.env.PUBLIC_URL}>
                     {this.props.navbar}
 
