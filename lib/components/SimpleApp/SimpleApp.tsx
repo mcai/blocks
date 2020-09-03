80f1063d04b0985d31d12e5811d677e24e537177
Min Cai <min.cai.china@gmail.com>
2020-09-03 09:20:11 +0800

    Update SimpleApp.tsx.

--- lib/components/SimpleApp/SimpleApp.tsx
+++ lib/components/SimpleApp/SimpleApp.tsx
@@ -2,23 +2,26 @@ import React, {Component} from "react";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {SimpleAppProps} from "./SimpleAppProps";
 import {SimpleAppState} from "./SimpleAppState";
+import {Container} from "react-bootstrap";
 
 export class SimpleApp extends Component<SimpleAppProps, SimpleAppState>{
     render() {
         return (
-            <BrowserRouter>
-                {this.props.navbar}
+            <Container>
+                <BrowserRouter>
+                    {this.props.navbar}
 
-                <Switch>
-                    {
-                        this.props.routes.map(route => (
-                            <Route exact path={route.path}>
-                                {route.page}
-                            </Route>
-                        ))
-                    }
-                </Switch>
-            </BrowserRouter>
+                    <Switch>
+                        {
+                            this.props.routes.map(route => (
+                                <Route exact path={route.path}>
+                                    {route.page}
+                                </Route>
+                            ))
+                        }
+                    </Switch>
+                </BrowserRouter>
+            </Container>
         );
     }
 }
