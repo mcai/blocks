69dbef5a79e9b2532ec1e7ce685d19da81a75e1a
Min Cai <min.cai.china@gmail.com>
2020-09-06 20:05:57 +0800

    Update SimpleActionsFieldType.tsx.

--- lib/fields/SimpleActionsField.tsx
+++ lib/fields/SimpleActionsField.tsx
@@ -1,20 +1,23 @@
-import React, { Fragment } from "react";
+import React, {Fragment} from "react";
 import {SimpleField} from "./SimpleField";
 import {Button} from "react-bootstrap";
+import {SimpleActionsFieldType} from "./SimpleActionsFieldType";
 
 export class SimpleActionsField implements SimpleField {
     readonly title: React.ReactNode;
 
     actions: {
         text: React.ReactNode,
-        urlFunc: (item: any) => string
+        urlFunc: (item: any) => string,
+        type?: SimpleActionsFieldType
     }[]
 
     constructor(
         title: React.ReactNode,
         actions: {
             text: React.ReactNode,
-            urlFunc: (item: any) => string
+            urlFunc: (item: any) => string,
+            type?: SimpleActionsFieldType
         }[]
     ) {
         this.title = title;
@@ -25,11 +28,28 @@ export class SimpleActionsField implements SimpleField {
         return (
             <Fragment>
                 {
-                    this.actions.map(action => (
-                        <Button variant={"primary"} href={action.urlFunc(item)}>
-                            {action.text}
-                        </Button>
-                    ))
+                    this.actions.map(action => {
+                        let buttonClass = "primary";
+
+                        if (action.type != undefined) {
+                            switch (action.type) {
+                                case SimpleActionsFieldType.none:
+                                    break;
+                                case SimpleActionsFieldType.danger:
+                                    buttonClass = "table-danger";
+                                    break;
+                                case SimpleActionsFieldType.warning:
+                                    buttonClass = "table-warning";
+                                    break;
+                            }
+                        }
+
+                        return (
+                            <Button variant={buttonClass} href={action.urlFunc(item)} className={"mr-3"}>
+                                {action.text}
+                            </Button>
+                        );
+                    })
                 }
             </Fragment>
         );
