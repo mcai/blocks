a92547024027293dffc6adc05861c0ae3fc5c01f
Min Cai <min.cai.china@gmail.com>
2020-09-08 19:03:53 +0800

    Update SimpleSshGitPullButton.

--- lib/components/SimpleSshButton/SimpleSshGitPullButton.tsx
+++ lib/components/SimpleSshButton/SimpleSshGitPullButton.tsx
@@ -2,26 +2,8 @@ import React, {Component} from "react";
 import {SimpleSshGitPullButtonProps} from "./SimpleSshGitPullButtonProps";
 import {SimpleSshGitPullButtonState} from "./SimpleSshGitPullButtonState";
 import {SimpleSshButton} from "./SimpleSshButton";
-import {promises} from "fs";
 
 export class SimpleSshGitPullButton extends Component<SimpleSshGitPullButtonProps, SimpleSshGitPullButtonState> {
-    tempFileName: string = "expect.config";
-
-    async componentDidMount() {
-        let lines = [
-            "#!/usr/bin/expect -f",
-            "spawn git pull",
-            "expect \"Password*:\"",
-            `send "${this.props.gitPassword}\r"`,
-            "expect eof"
-        ];
-
-        await promises.writeFile(
-            this.tempFileName,
-            lines.join('\n')
-        );
-    }
-
     render() {
         return <SimpleSshButton
             host={this.props.host}
@@ -29,12 +11,14 @@ export class SimpleSshGitPullButton extends Component<SimpleSshGitPullButtonProp
             username={this.props.sshUsername}
             password={this.props.sshPassword}
             workingDirectory={this.props.workingDirectory}
-            filesToUpload={[{
-                localFileName: this.tempFileName,
-                remoteFileName: this.tempFileName
-            }]}
             commands={[
-                "expect" // TODO
+                "/usr/bin/expect <<EOF",
+                "expect \"Username for 'https://github.com':\"",
+                `send "${this.props.gitUsername}\r`,
+                "expect \"Password for 'https://$0@github.com':\"",
+                `send "${this.props.gitPassword}\r"`,
+                "expect EOF",
+                "EOF",
             ]}
         />;
     }
