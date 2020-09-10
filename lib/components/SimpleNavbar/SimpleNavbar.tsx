08b97175171766308d3c96fa79abb6dd3231c9b0
Min Cai <min.cai.china@gmail.com>
2020-09-10 22:46:09 +0800

    Update SimpleNavbar.tsx.

--- lib/components/SimpleNavbar/SimpleNavbar.tsx
+++ lib/components/SimpleNavbar/SimpleNavbar.tsx
@@ -10,31 +10,32 @@ export class SimpleNavbar extends Component<SimpleNavbarProps, any> {
     render() {
         return (
             <Navbar bg="primary" variant="dark" className={"mb-3"}>
-                    <Navbar.Brand href={this.props.brand.href}>{this.props.brand.title}</Navbar.Brand>
-                    <Nav className="mr-auto">
-                        {
-                            this.props.sections
-                                .filter(section => section.visible == undefined || section.visible)
-                                .map(section => (
-                                    <NavDropdown title={section.title} id={section.id}>
-                                        {
-                                            section.items
-                                                .filter(item => item.visible == undefined || item.visible)
-                                                .map(item => (
-                                                    <NavDropdown.Item
-                                                        href={item.href}
-                                                        onClick={item.onClick}
-                                                        key={item.key}
-                                                    >
-                                                        {item.title}
-                                                    </NavDropdown.Item>
-                                                ))
-                                        }
-                                    </NavDropdown>
-                                ))
-                        }
-                    </Nav>
-                </Navbar>
+                <Navbar.Brand href={this.props.brand.href}>{this.props.brand.title}</Navbar.Brand>
+                <Nav className="mr-auto mr-3">
+                    {
+                        this.props.sections
+                            .filter(section => section.visible == undefined || section.visible)
+                            .map(section => (
+                                <NavDropdown title={section.title} id={section.id}>
+                                    {
+                                        section.items
+                                            .filter(item => item.visible == undefined || item.visible)
+                                            .map(item => (
+                                                <NavDropdown.Item
+                                                    href={item.href}
+                                                    onClick={item.onClick}
+                                                    key={item.key}
+                                                >
+                                                    {item.title}
+                                                </NavDropdown.Item>
+                                            ))
+                                    }
+                                </NavDropdown>
+                            ))
+                    }
+                </Nav>
+                {this.props.extra}
+            </Navbar>
         );
     }
 }
--- lib/components/SimpleNavbar/SimpleNavbarProps.tsx
+++ lib/components/SimpleNavbar/SimpleNavbarProps.tsx
@@ -2,10 +2,13 @@ import React from "react";
 
 export interface SimpleNavbarProps {
     brand: { title?: React.ReactNode, href?: string };
+
     sections: {
         id: string,
         title: string,
         visible?: boolean,
         items: { key: string, title?: React.ReactNode, href?: string, onClick?: () => void, visible?: boolean }[]
     }[];
+
+    extra?: React.ReactNode;
 }
