475cf7cef89cd68ab9f2400887b6c0505e987b07
Min Cai <min.cai.china@gmail.com>
2020-09-23 22:54:44 +0800

    Add testing framework integration.

---
+++ lib/inputs/SimpleHexInput/tests/SimpleHexInput.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Form, Formik, FormikProps} from "formik";
+import {SimpleHexInput} from "../SimpleHexInput";
+import {Button} from "react-bootstrap";
+import renderer from 'react-test-renderer';
+
+test('renders without crashing', () => {
+    let component = renderer.create(
+        <Formik
+            initialValues={{
+                name: "3F2D"
+            }}
+            onSubmit={values => {
+                console.log(JSON.stringify(values));
+            }}
+        >
+            {(props: FormikProps<any>) => (
+                <Form>
+                    <SimpleHexInput label={"名称"} name={"name"}/>
+                    <Button variant={"primary"} type="submit">提交</Button>
+                </Form>
+            )}
+        </Formik>
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
