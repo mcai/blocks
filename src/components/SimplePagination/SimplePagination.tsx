3b9fcc11a6403a8d9aa2cb11006545e8903f25e1
Min Cai <min.cai.china@gmail.com>
2020-09-28 18:09:58 +0800

    Update SimplePagination.tsx.

--- src/components/SimplePagination/SimplePagination.tsx
+++ src/components/SimplePagination/SimplePagination.tsx
@@ -18,12 +18,6 @@ export class SimplePagination extends Component<SimplePaginationProps, any> {
         return (
             <Fragment>
                 {
-                    this.props.count > 0 && <label>
-                        第 {this.props.pageNum + 1} / {this.props.pageCount} 页 (共 {this.props.count} 项, 本页显示 {this.props.pageSize} 项)
-                    </label>
-                }
-
-                {
                     this.props.pageCount > 1 && <Fragment>
                         <SimpleButton onClick={() => this.props.onClick(0)}>首页</SimpleButton>
 
@@ -37,6 +31,12 @@ export class SimplePagination extends Component<SimplePaginationProps, any> {
 
                         <SimpleButton onClick={() => this.props.onClick(this.props.pageCount - 1)}>尾页</SimpleButton>
                     </Fragment>
+                }
+
+                {
+                    this.props.count > 0 && <label>
+                        第 {this.props.pageNum + 1} / {this.props.pageCount} 页 (共 {this.props.count} 项, 本页显示 {this.props.pageSize} 项)
+                    </label>
                 }
             </Fragment>
         );
