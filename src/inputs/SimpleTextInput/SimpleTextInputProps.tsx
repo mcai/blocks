fb6e17c31c1f53e5fa520f4f63ab60bf809cdd69
Min Cai <min.cai.china@gmail.com>
2020-10-02 17:12:37 +0800

    Update SimpleTextInputProps.tsx.

--- src/inputs/SimpleTextInput/SimpleTextInputProps.tsx
+++ src/inputs/SimpleTextInput/SimpleTextInputProps.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 export interface SimpleTextInputProps {
     label: React.ReactNode;
+
     name: string;
-    placeholder?: string
-    password?: boolean
+
+    placeholder?: string;
+
+    password?: boolean;
 }
