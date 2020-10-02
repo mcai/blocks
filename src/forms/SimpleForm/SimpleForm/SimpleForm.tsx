db4db86bd3feb224105bb762a70201f8e19fda5d
Min Cai <min.cai.china@gmail.com>
2020-10-02 20:15:40 +0800

    Update SimpleForm.tsx.

--- src/forms/SimpleForm/SimpleForm/SimpleForm.tsx
+++ src/forms/SimpleForm/SimpleForm/SimpleForm.tsx
@@ -28,25 +28,23 @@ export class SimpleForm extends Component<SimpleFormProps, SimpleFormState> {
 
     render() {
         return (
-            <Fragment>
-                <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => this.onSubmit(e)}>
-                    {
-                        React.Children.map(this.props.children, input => React.isValidElement(input)
-                            ? React.cloneElement(input, {
-                                values: this.state,
-                                onUpdate: (name: string, value: string) => {
-                                    this.onUpdate(name, value);
-                                }
-                            })
-                            : input
-                        )
-                    }
+            <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => this.onSubmit(e)}>
+                {
+                    React.Children.map(this.props.children, input => React.isValidElement(input)
+                        ? React.cloneElement(input, {
+                            values: this.state,
+                            onUpdate: (name: string, value: string) => {
+                                this.onUpdate(name, value);
+                            }
+                        })
+                        : input
+                    )
+                }
 
-                    <SimpleRow>
-                        <SimpleButton type="submit">{this.props.submitButtonText ?? "提交"}</SimpleButton>
-                    </SimpleRow>
-                </form>
-            </Fragment>
+                <SimpleRow>
+                    <SimpleButton type="submit">{this.props.submitButtonText ?? "提交"}</SimpleButton>
+                </SimpleRow>
+            </form>
         );
     }
 }
