33ef3d890b23f67fa40f1db3ceab96bba09cb6ed
Min Cai <min.cai.china@gmail.com>
2020-10-02 19:46:07 +0800

    Remove SimpleFormDemoState.

--- src/apps/SimpleFormDemo/SimpleFormDemo.tsx
+++ src/apps/SimpleFormDemo/SimpleFormDemo.tsx
@@ -1,23 +1,12 @@
 import React, {Component} from "react";
 import {SimpleForm} from "../../forms/SimpleForm/SimpleForm/SimpleForm";
 import {SimpleFormTextInput} from "../../forms/SimpleForm/Fields/SimpleFormTextInput/SimpleFormTextInput";
-import {SimpleFormDemoState} from "./SimpleFormDemoState";
 import {SimpleFormDemoProps} from "./SimpleFormDemoProps";
 
-export class SimpleFormDemo extends Component<SimpleFormDemoProps, SimpleFormDemoState> {
-    constructor(props: SimpleFormDemoProps) {
-        super(props);
-
-        this.state = {
-            ...this.props.initialValues
-        };
-    }
-
+export class SimpleFormDemo extends Component<SimpleFormDemoProps, any> {
     render() {
         return (
-            <SimpleForm initialValues={this.state} onSubmit={values => {
-                this.setState(values);
-
+            <SimpleForm initialValues={this.props.initialValues} onSubmit={values => {
                 alert(`SimpleFormDemo.onSubmit: values=${JSON.stringify(values)}`);
             }}>
                 <SimpleFormTextInput label={"User ID"} name={"userId"}/>
--- src/apps/SimpleFormDemo/SimpleFormDemoState.tsx
+++
@@ -1,3 +0,0 @@
-export interface SimpleFormDemoState {
-    [name: string]: string;
-}
