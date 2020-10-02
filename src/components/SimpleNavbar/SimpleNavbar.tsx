e44e2b9c00a8b07f2a5e7dbec7118cc695c88829
Min Cai <min.cai.china@gmail.com>
2020-10-02 22:49:58 +0800

    Add SimpleNavbar.scss.

--- src/components/SimpleNavbar/SimpleNavbar.tsx
+++ src/components/SimpleNavbar/SimpleNavbar.tsx
@@ -3,105 +3,47 @@ import {SimpleNavbarProps} from "./SimpleNavbarProps";
 
 import "./SimpleNavbar.scss";
 
-function SimpleNavbarClass(props: any) {
-    return (
-        <ul className={"SimpleNavbar"}>
-            {props.children}
-        </ul>
-    );
-}
-
-function SimpleNavbarBrandClass(props: any) {
-    return (
-        <li className={"SimpleNavbarBrand"}>
-            {props.children}
-        </li>
-    );
-}
-
-class SimpleNavDropDownItemClass extends Component<any> {
-    render() {
-        return (
-            <div className={"SimpleNavDropDownItem"}>
-                {this.props.children}
-            </div>
-        );
-    }
-}
-
-function SimpleDropDownContentClass(props: any) {
-    return (
-        <div className={"SimpleDropDownContent"}>
-            {props.children}
-        </div>
-    );
-}
-
-function SimpleNavDropdownClass(props: any) {
-    return (
-        <li className={"SimpleNavDropdown"}>
-            {props.children}
-        </li>
-    );
-}
-
-function SimpleNavbarBrandItemClass(props: any) {
-    return (
-        <a className={"SimpleNavbarBrandItem"}>
-            {props.children}
-        </a>
-    );
-}
-
-function SimpleDropDownContentItemClass(props: any) {
-    return (
-        <a className={"SimpleDropDownContentItem"}>
-            {props.children}
-        </a>
-    );
-}
-
 export class SimpleNavbar extends Component<SimpleNavbarProps, any> {
     render() {
         return (
-            <SimpleNavbarClass>
-                <SimpleNavbarBrandClass>
-                    <SimpleNavbarBrandItemClass href={this.props.brand.href}>{this.props.brand.title}</SimpleNavbarBrandItemClass>
-                </SimpleNavbarBrandClass>
+            <ul className={"SimpleNavbar"}>
+                <li className={"SimpleNavbarBrand"}>
+                    <a className={"SimpleNavbarBrandItem"} href={this.props.brand.href}>{this.props.brand.title}</a>
+                </li>
 
                 {
                     this.props.sections
                         .filter(section => section.visible === undefined || section.visible)
                         .map(section => {
                             return (
-                                <SimpleNavDropdownClass key={section.id} style={{
+                                <li className={"SimpleNavDropdown"} key={section.id} style={{
                                     float: section.rightAligned ? "right" : "left"
                                 }}>
-                                    <SimpleNavDropDownItemClass>
+                                    <div className={"SimpleNavDropDownItem"}>
                                         {section.title}
-                                    </SimpleNavDropDownItemClass>
-                                    <SimpleDropDownContentClass>
+                                    </div>
+                                    <div className={"SimpleDropDownContent"}>
                                         {
                                             section.items
                                                 .filter(item => item.visible === undefined || item.visible)
                                                 .map(item => (
-                                                    <SimpleDropDownContentItemClass
+                                                    <a className={"SimpleDropDownContentItem"}
                                                         href={item.href}
                                                         onClick={item.onClick}
                                                         key={item.key}
                                                     >
                                                         {item.title}
-                                                    </SimpleDropDownContentItemClass>
+                                                    </a>
                                                 ))
                                         }
-                                    </SimpleDropDownContentClass>
-                                </SimpleNavDropdownClass>
+                                    </div>
+                                </li>
                             );
                         })
                 }
 
                 {this.props.extra}
-            </SimpleNavbarClass>
+            </ul>
         );
     }
 }
