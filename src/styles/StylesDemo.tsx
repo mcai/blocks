19efe23a2c491a443cb25e7e9436a193f8f07305
Min Cai <min.cai.china@gmail.com>
2020-10-03 21:27:41 +0800

    Update Simple.scss.

--- src/styles/StylesDemo.tsx
+++ src/styles/StylesDemo.tsx
@@ -3,21 +3,59 @@ import React, {Fragment} from "react";
 export class StylesDemo extends React.Component<any> {
     render() {
         return (
-            <div className="simple-card simple-row">
-                <div className="simple-center">
-                    <h1 className="simple-h1">
-                        一级标题
-                    </h1>
-
-                    <h2 className="simple-h2">
-                        二级标题
-                    </h2>
-
-                    <p className="simple-p">
-                        段落
-                    </p>
-
+            <Fragment>
+                <div className="simple-container">
                     <div className="simple-row">
+                        <div className="simple-col">
+                            hello world 1
+                        </div>
+                        <div className="simple-col">
+                            hello world 2
+                        </div>
+                        <div className="simple-col">
+                            hello world 3
+                        </div>
+                    </div>
+                    <div className="simple-row">
+                        <div className="simple-col">
+                            hello world 4
+                        </div>
+                        <div className="simple-col">
+                            hello world 5
+                        </div>
+                        <div className="simple-col">
+                            hello world 6
+                        </div>
+                    </div>
+                    <div className="simple-row">
+                        <div className="simple-col">
+                            hello world 7
+                        </div>
+                        <div className="simple-col">
+                            hello world 8
+                        </div>
+                        <div className="simple-col">
+                            hello world 9
+                        </div>
+                    </div>
+                </div>
+
+                <div className="simple-card">
+                    <div className="simple-center">
+                        <h1 className="simple-h1">
+                            一级标题
+                        </h1>
+
+                        <h2 className="simple-h2">
+                            二级标题
+                        </h2>
+
+                        <p className="simple-p">
+                            段落
+                        </p>
+                    </div>
+
+                    <div>
                         <div className="simple-left">
                             <Fragment>
                                 <button className="simple-button" onClick={() => alert("hello")}>
@@ -59,7 +97,7 @@ export class StylesDemo extends React.Component<any> {
                         </div>
                     </div>
                 </div>
-            </div>
+            </Fragment>
         );
     }
 }
