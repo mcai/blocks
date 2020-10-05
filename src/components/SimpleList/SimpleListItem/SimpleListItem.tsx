f3c4b20ec939bc999be056d9b0035900104afe98
Min Cai <min.cai.china@gmail.com>
2020-10-05 13:16:08 +0800

    Update SimpleList.

--- src/components/SimpleList/SimpleListItem/SimpleListItem.tsx
+++ src/components/SimpleList/SimpleListItem/SimpleListItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import {SimpleListItemProps} from "./SimpleListItemProps";
 import {SimpleFormTextInput} from "../../../forms/SimpleForm/Fields/SimpleFormTextInput/SimpleFormTextInput";
 
@@ -11,38 +11,48 @@ export class SimpleListItem extends React.Component<SimpleListItemProps, any> {
 
     render() {
         return (
-            <div className="simple-row">
-                <b className="simple-input-label">
-                    {this.props.index + 1}. {this.props.item.description} {this.props.item.values ? ": " : ""}
-                </b>
+            <Fragment>
+                <div className="simple-row">
+                    <b className="simple-input-label">
+                        {this.props.index + 1}. {this.props.item.description} {this.props.item.values ? ": " : ""}
+                    </b>
 
-                <div className="simple-input">
-                    {
-                        Object.keys(this.props.item.values).map(key =>
-                            <SimpleFormTextInput
-                                key={key}
-                                label={this.props.item.values[key].label}
-                                name={key}
-                                placeholder={this.props.item.values[key].label}
-                                password={false}
-                                values={{
-                                    key: this.props.item.values[key].value
-                                }}
-                                onUpdate={((name, value) => {
-                                    this.onUpdate(key, value);
-                                })}
-                            />
-                        )
-                    }
-
-                    <button
-                        className="simple-button"
-                        onClick={() => this.props.onRemove(this.props.index)}
-                    >
-                        删除
-                    </button>
+                    <div className="simple-input">
+                        &nbsp;
+                    </div>
                 </div>
-            </div>
+
+                {
+                    Object.keys(this.props.item.values).map(key =>
+                        <SimpleFormTextInput
+                            key={key}
+                            label={this.props.item.values[key].label}
+                            name={key}
+                            placeholder={this.props.item.values[key].label}
+                            password={false}
+                            values={{
+                                key: this.props.item.values[key].value
+                            }}
+                            onUpdate={((name, value) => {
+                                this.onUpdate(key, value);
+                            })}
+                        />
+                    )
+                }
+
+                <div className="simple-row">
+                    <span className="simple-input-label">&nbsp;</span>
+
+                    <div className="simple-input">
+                        <button
+                            className="simple-button"
+                            onClick={() => this.props.onRemove(this.props.index)}
+                        >
+                            删除
+                        </button>
+                    </div>
+                </div>
+            </Fragment>
         );
     }
 }
