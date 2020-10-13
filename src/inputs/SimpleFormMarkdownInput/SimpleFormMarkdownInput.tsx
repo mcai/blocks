1dbf0310f6777a18ec33b643d5030c8c7f56ecf3
Min Cai <min.cai.china@gmail.com>
2020-10-14 00:30:08 +0800

    Add SimpleFormMarkdownInput.tsx.

---
+++ src/inputs/SimpleFormMarkdownInput/SimpleFormMarkdownInput.tsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { SimpleFormMarkdownInputProps } from "./SimpleFormMarkdownInputProps";
+import MDEditor from "@uiw/react-md-editor";
+
+export class SimpleFormMarkdownInput extends Component<SimpleFormMarkdownInputProps, any> {
+    onUpdate(value: any) {
+        this.props.onUpdate?.(this.props.name ?? "", value);
+
+        console.log(`SimpleFormMarkdownInput.onUpdate: name=${this.props.name}, value=${value}`);
+    }
+
+    render() {
+        console.log(
+            `SimpleFormMarkdownInput.render: name=${this.props.name}, this.props.values=${JSON.stringify(
+                this.props.values,
+            )}, value=${this.props.values?.[this.props.name ?? ""]}`,
+        );
+
+        return (
+            <div className="simple-row">
+                <span className="simple-input-label">{this.props.label}: </span>
+
+                {this.props.readonly !== undefined && this.props.readonly ? (
+                    <MDEditor.Markdown className="simple-input" source={this.props.values?.[this.props.name ?? ""]} />
+                ) : (
+                    <MDEditor
+                        className="simple-input"
+                        value={this.props.values?.[this.props.name ?? ""]}
+                        onChange={(value) => this.onUpdate(value)}
+                    />
+                )}
+            </div>
+        );
+    }
+}
---
+++ src/inputs/SimpleFormMarkdownInput/SimpleFormMarkdownInputProps.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export interface SimpleFormMarkdownInputProps {
+    label?: React.ReactNode;
+
+    name?: string;
+
+    values?: { [name: string]: any };
+
+    onUpdate?: (name: string, value: any) => void;
+
+    readonly?: boolean;
+}
