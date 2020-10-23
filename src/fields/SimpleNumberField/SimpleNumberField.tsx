89c5e28d125c9d50f60464c45b65e196c981e699
Min Cai <min.cai.china@gmail.com>
2020-10-23 14:21:06 +0800

    Fix a bug in SimpleNumberField.tsx.

--- src/fields/SimpleNumberField/SimpleNumberField.tsx
+++ src/fields/SimpleNumberField/SimpleNumberField.tsx
@@ -4,10 +4,6 @@ import { SimpleNumberFieldProps } from "./SimpleNumberFieldProps";
 export class SimpleNumberField extends Component<SimpleNumberFieldProps, any> {
     render() {
         const value = this.props.values?.[this.props.name ?? ""];
-
-        if (value !== undefined && typeof value === "number") {
-            return value.toFixed(this.props.fractionDigits) ?? "";
-        }
-        return "";
+        return Number(value ?? 0).toFixed(this.props.fractionDigits);
     }
 }
