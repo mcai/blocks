53834e570cbbe7a8772a19370a52926e9ccf89f2
Min Cai <min.cai.china@gmail.com>
2020-11-02 19:50:59 +0800

    Update SimpleDxfDrawView.tsx.

--- src/components/SimpleDxfDrawView/SimpleDxfLineView/SimpleDxfLineView.tsx
+++ src/components/SimpleDxfDrawView/SimpleDxfLineView/SimpleDxfLineView.tsx
@@ -3,13 +3,21 @@ import { SimpleDxfLineViewProps } from "./SimpleDxfLineViewProps";
 import { SimpleDxfLineViewState } from "./SimpleDxfLineViewState";
 import { Line } from "react-konva";
 import { SimpleDxfDrawColorExtensions } from "../SimpleDxfDrawColorExtensions";
+import chroma from "chroma-js";
 
 export class SimpleDxfLineView extends Component<SimpleDxfLineViewProps, SimpleDxfLineViewState> {
     render(): React.ReactNode {
-        const stroke = SimpleDxfDrawColorExtensions.getDescription(this.props.drawObject.stroke);
+        let stroke = SimpleDxfDrawColorExtensions.getDescription(this.props.drawObject.stroke);
         let fill = SimpleDxfDrawColorExtensions.getDescription(this.props.drawObject.fill);
 
-        if (this.props.highlighted || this.props.drawPart.needRefinement) {
+        if (this.props.drawPart.needRefinement) {
+            const color2 = "#ffa62b";
+
+            stroke = chroma.blend(stroke, color2, "darken").hex();
+            fill = chroma.blend(fill, color2, "darken").hex();
+        }
+
+        if (this.props.highlighted) {
             fill = stroke;
         }
 
