1482cb24fba164b380569d2bba37c40b0c32c875
Min Cai <min.cai.china@gmail.com>
2021-01-03 14:44:32 +0800

    Update tailwind stuffs.

--- src/index.tsx
+++ src/index.tsx
@@ -8,10 +8,16 @@ import "./tailwind.output.css";
 import { SimpleTodoApp } from "./apps/SimpleTodoApp/SimpleTodoApp";
 import { SimpleFormDemo } from "./apps/SimpleFormDemo/SimpleFormDemo";
 
+export class SimpleTestButton extends React.Component {
+    render() {
+        return <button className="bg-red-500 border-2 border-blue-400 px-3 py-2 m-4 hover:underline">hello</button>;
+    }
+}
+
 ReactDOM.render(
     <React.StrictMode>
         <div>
-            <button className="bg-red-500 border-2 border-blue-400 px-3 py-2 m-4 hover:underline">hello</button>
+            <SimpleTestButton />
             <SimpleFormDemo />
             <SimpleTodoApp />
         </div>
