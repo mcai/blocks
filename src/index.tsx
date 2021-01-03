5c05c4abae5b98d202614a9cd7fa21749376510a
Min Cai <min.cai.china@gmail.com>
2021-01-03 14:04:30 +0800

    Update tailwind stuffs.

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./simple.scss";
-
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./tailwind.output.css";
 
 import { SimpleTodoApp } from "./apps/SimpleTodoApp/SimpleTodoApp";
 import { SimpleFormDemo } from "./apps/SimpleFormDemo/SimpleFormDemo";
 
 ReactDOM.render(
     <React.StrictMode>
-        <div className="SimpleTheme3">
+        <div>
+            <button className="bg-red-500 border-2 border-blue-400 px-3 py-2 m-4 hover:underline">hello</button>
             <SimpleFormDemo />
             <SimpleTodoApp />
         </div>
