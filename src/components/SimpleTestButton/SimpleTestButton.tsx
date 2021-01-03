933e700de9735497378d8471302c8e9897e072ab
Min Cai <min.cai.china@gmail.com>
2021-01-03 14:53:40 +0800

    Update tailwind stuffs.

---
+++ src/components/SimpleTestButton/SimpleTestButton.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class SimpleTestButton extends React.Component {
+    render() {
+        return <button className="bg-red-500 border-2 border-blue-400 px-3 py-2 m-4 hover:underline">hello</button>;
+    }
+}
