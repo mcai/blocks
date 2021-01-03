16d8c37223c68b6eadfbfbe4b63aef2df0887cf1
Min Cai <min.cai.china@gmail.com>
2021-01-04 01:26:13 +0800

    Update tailwind stuffs.

--- src/components/SimpleTestButton/SimpleTestButton.tsx
+++ src/components/SimpleTestButton/SimpleTestButton.tsx
@@ -2,6 +2,10 @@ import React from "react";
 
 export class SimpleTestButton extends React.Component {
     render() {
-        return <button className="bg-red-500 border-2 border-blue-400 px-3 py-2 m-4 hover:underline">hello</button>;
+        return (
+            <button className="tw-bg-red-500 tw-border-2 tw-border-blue-400 tw-px-3 tw-py-2 tw-m-4 hover:tw-underline">
+                hello
+            </button>
+        );
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 // eslint-disable-next-line no-undef
 module.exports = {
+    prefix: "tw-",
     purge: {
         content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     },
     darkMode: false, // or 'media' or 'class'
     theme: {
+        screens: {
+            sm: "576px",
+            md: "768px",
+            lg: "992px",
+            xl: "1200px",
+        },
         extend: {},
     },
     variants: {
