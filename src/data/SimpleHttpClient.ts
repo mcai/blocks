5df212c2a16a8c13bc86900a1cf00d57a04abe96
Min Cai <min.cai.china@gmail.com>
2021-01-10 01:36:37 +0800

    Update SimpleHttpClient.ts.

--- src/data/SimpleHttpClient.ts
+++ src/data/SimpleHttpClient.ts
@@ -4,21 +4,49 @@ import { stringifyUrl } from "query-string";
 import urljoin from "url-join";
 
 export class SimpleHttpClient {
-    static async call(url: string, method: SimpleHttpClientMethod, params: any): Promise<any | undefined> {
+    static async call(
+        url: string,
+        method: SimpleHttpClientMethod,
+        params: any,
+        onProgress?: (loaded: number, percent?: number, total?: number) => void,
+    ): Promise<any | undefined> {
         const response =
             method === SimpleHttpClientMethod.post
-                ? await request.post(url).type("form").send(params)
-                : await request.get(url).query(params);
+                ? await request
+                      .post(url)
+                      .type("form")
+                      .send(params)
+                      .on("progress", (event) => {
+                          onProgress?.(event.loaded, event.percent, event.total);
+                      })
+                : await request
+                      .get(url)
+                      .query(params)
+                      .on("progress", (event) => {
+                          onProgress?.(event.loaded, event.percent, event.total);
+                      });
 
         return response.body ?? response.text;
     }
 
-    static async get(baseUrl: string, resource: string, action: string, params: any): Promise<any | undefined> {
-        return await this.call(urljoin(baseUrl, resource, action), SimpleHttpClientMethod.get, params);
+    static async get(
+        baseUrl: string,
+        resource: string,
+        action: string,
+        params: any,
+        onProgress?: (loaded: number, percent?: number, total?: number) => void,
+    ): Promise<any | undefined> {
+        return await this.call(urljoin(baseUrl, resource, action), SimpleHttpClientMethod.get, params, onProgress);
     }
 
-    static async post(baseUrl: string, resource: string, action: string, params: any): Promise<any | undefined> {
-        return await this.call(urljoin(baseUrl, resource, action), SimpleHttpClientMethod.post, params);
+    static async post(
+        baseUrl: string,
+        resource: string,
+        action: string,
+        params: any,
+        onProgress?: (loaded: number, percent?: number, total?: number) => void,
+    ): Promise<any | undefined> {
+        return await this.call(urljoin(baseUrl, resource, action), SimpleHttpClientMethod.post, params, onProgress);
     }
 
     static buildUrl(baseUrl: string, resource: string, action: string, query: any): string {
